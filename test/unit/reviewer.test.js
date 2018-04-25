3c3fea470913a248cdfd3b2dbab729523aa115ca
CharlyWelch <blwbiology@gmail.com>
2018-04-25 09:25:11 -0700

    add generateHash and comparePassword to reviewer

--- test/unit/reviewer.test.js
+++ test/unit/reviewer.test.js
@@ -2,17 +2,39 @@ const { assert } = require('chai');
 const Reviewer = require('../../lib/models/Reviewer');
 
 describe('reviewer model', () => {
-    it('reviewer is a valid model', () => {
-        const data = {
-            name: 'Roger Ebert',
-            company: 'roger-ebert.com'
-        };
 
-        const reviewer = new Reviewer(data);
+    const data = {
+        name: 'Roger Ebert',
+        company: 'roger-ebert.com',
+        email: 'roger@roger.com',
+        roles: ['admin']
+    };
+
+    const password = '1234';
+
+    let reviewer = null;
+
+    beforeEach(() => {
+        reviewer = new Reviewer(data);
         data._id = reviewer._id;
-        assert.deepEqual(reviewer.toJSON(), data);
+        reviewer.generateHash(password);
+        data.hash = reviewer.hash;
     });
 
+    
+    it('generates a hash from password', () => {
+        assert.ok(reviewer.hash);
+        assert.notEqual(reviewer.hash, password);
+    });
+
+    it('compares password to hash', () => {
+        assert.isOk(reviewer.comparePassword(password));
+    });
+
+    it('reviewer is a valid model', () => {
+        assert.deepEqual(reviewer.toJSON(), data);
+    });
+                
     it('name and company are required', () => {
         const reviewer = new Reviewer({});
         const { errors } = reviewer.validateSync();
